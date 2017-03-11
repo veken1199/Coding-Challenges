df6fe7f782386772d056adae740c93e12fff0b91
veken1199 <super_veko@yahoo.com>
2017-03-10 22:57:45 -0500

    improved the interface

--- Reverse Integer/src/Solution.java
+++ Reverse Integer/src/Solution.java
@@ -11,14 +11,10 @@
 
 public class Solution {
 	public static long reversed(long rev, int x){
-		if (rev>Integer.MAX_VALUE || rev<Integer.MIN_VALUE){
-			return 0;
-		}
-		
-		else if (x==0){
+		if (x==0){
 			return rev;
 		}
-		
+
 		else{
 			return reversed((rev*10 + x%10), x/10);
 		}
@@ -26,13 +22,19 @@ public class Solution {
 	
 	
 	public static int reverse(int x) {
-		return (int)reversed(0,x);
+		long rev = reversed(0,x);
+		if (rev>Integer.MAX_VALUE || rev<Integer.MIN_VALUE){
+			return 0;
+		}
+		else {
+			return (int)rev;
+		}
 	}
     
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(reverse(1534236469));
+		System.out.println(reverse(15342369));
 	}
 
 }
